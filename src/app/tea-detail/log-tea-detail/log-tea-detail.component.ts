4fe70eef0b3cf533d6b154087521f0566d2af74b
BordyV <56049773+BordyV@users.noreply.github.com>
2022-01-29 22:05:42 +0100

    feat: ajout des commentaires via le détail du thé

--- src/app/tea-detail/log-tea-detail/log-tea-detail.component.ts
+++ src/app/tea-detail/log-tea-detail/log-tea-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Log } from 'src/app/models/log.model';
+import { LogService } from 'src/app/services/log.service';
 
 @Component({
   selector: 'app-log-tea-detail',
@@ -10,9 +12,39 @@ export class LogTeaDetailComponent implements OnInit {
   //props in
   @Input() logs: Log[] = [];
 
-  constructor() { }
+  constructor(private logService: LogService, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
+   addCommentaire(log: Log) {
+    this.logService.updateLog(log).subscribe({
+      next: (v) => {
+        if (v.nModified > 0) {
+          this.openSnackBar('Commentaire bien enregistré', 'fermer');
+        }
+        else {
+          this.openSnackBar('Problème lors de l\'enregistrement', 'fermer');
+        }
+      },
+      error: (e) => {
+        this.openSnackBar(e.message, 'fermer');
+
+      },
+      complete: () => {
+
+      }
+    });
+  }
+
+  handleInput(event: KeyboardEvent): void {
+    event.stopPropagation();
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2 * 1000,
+      verticalPosition: 'top',
+    });
+  }
 }
